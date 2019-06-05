ed5aa07f98f1c313e35a1a16acc51c69bb30b4ec
Zhipeng-Chang <zchang0302@gmail.com>
2019-06-05 16:33:04 -0600

    bug fix for config example

---
+++ android/app/src/main/java/ca/edmonton/jazz/ConfigExample.java
@@ -0,0 +1,11 @@
+package ca.edmonton.jazz;
+
+/**
+ * Created by jarrew on 12/15/2017.
+ */
+
+public class ConfigExample {
+    public static final String group = "group";
+    public static final String license = "license";
+    public static final String server_client_id = "server_client_id";
+}
