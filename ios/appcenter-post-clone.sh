83a32b117fbe59e87ad12aa3473051561961a641
Zhipeng-Chang <zchang0302@gmail.com>
2019-06-05 15:48:43 -0600

    Update appcenter-post-clone.sh

--- ios/appcenter-post-clone.sh
+++ ios/appcenter-post-clone.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Example: Change bundle name of an iOS app for non-production
+# Create Config.swift file after cloned from Github
 echo "Start changing ENV VAR"
 awk -v group="\"$GROUP"\"";" -v license="\"$LICENCES"\"";" -v clientID="\"$SERVER_CLIENT_ID"\"";"\
  '{if ($3=="license") {$5=license; print} else if ($3=="group") {$5=group; print} else if ($3=="clientID") {$5=clientID; print} else {print}}' JazziOS/Config.swift.example.swift > JazziOS/Config.swift
